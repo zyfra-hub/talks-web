95e173008c9104e29e8096f5868797766f688231
Matthew Hodgson <matthew@matrix.org>
2016-02-25 01:17:30 +0100

    karma experiments

---
+++ src/tests/components/structures/MatrixChat-test.js
@@ -0,0 +1,12 @@
+var React = require('react/addons'),
+    TestUtils = React.addons.TestUtils,
+    expect = require('expect'),
+    ScatterPlot = require('');
+
+var sdk = require("matrix-react-sdk");
+sdk.loadSkin(require('../../../component-index'));
+
+var MatrixChat = sdk.getComponent('structures.MatrixChat');
+
+describe('MatrixChat', function () {
+});
