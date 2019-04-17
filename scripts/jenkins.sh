a5592d5c1f261c95558178582cebeecb6e268609
Travis Ralston <travpc@gmail.com>
2019-04-17 12:18:05 -0600

    Update jenkins.sh

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm use 10
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+#nvm use 10
 
 set -x
 
